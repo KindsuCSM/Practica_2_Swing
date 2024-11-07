75be4fe5062597afef830495821a634ee7bd63e0
KindsuCSM <crismngk@gmail.com>
2024-11-07 18:47:19 +0100

    PanelAcercaDe finiquitado

--- src/view/PanelAcercaDe.java
+++ src/view/PanelAcercaDe.java
@@ -1,13 +1,38 @@
 package view;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 public class PanelAcercaDe extends JPanel {
-
 	private static final long serialVersionUID = 1L;
-
+	public JLabel lblInformacion, lblInformacionContenido, lblAutores, lblAutoresJulian, lblAutoresCristina;
 	public PanelAcercaDe() {
+		setLayout(new GridLayout(0, 1, 10, 10));
+		initComponents();
+	}
+	private void initComponents() {
+		lblInformacion = new JLabel("Información sobre la aplicación: ");
+		lblInformacion.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		add(lblInformacion);
+
+		lblInformacionContenido = new JLabel();
+		lblInformacionContenido.setText("<html><p> &emsp;Esta aplicación trabaja sobre una lista de empleados y nos permite mostrarla por pantalla o agregar un empleado si lo requiere el usuario.</p></html>");
+		lblInformacionContenido.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		add(lblInformacionContenido);
+
+		lblAutores = new JLabel("Autores: ");
+		lblAutores.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		add(lblAutores);
+
+		lblAutoresJulian = new JLabel("<html><p> &emsp; - Julian Cabanillas Blanco</p></html>");
+		lblAutoresJulian.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		add(lblAutoresJulian);
+
+		lblAutoresCristina = new JLabel("<html><p> &emsp; - Cristina Sánchez Moreno</p></html>");
+		lblAutoresCristina.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		add(lblAutoresCristina);
 
 	}
+
 
 }
